364288c5cc6f2416c411993e191c16056333e852
abeldoescoding <abeldoescoding@gmail.com>
2024-01-17 16:29:08 -0500

    used html, css and js to build banner under game to display whose turn it is

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var turnNumber = 0;
 
+/* these variables need to become one array, should have done that in the first placce*/
 const topLeft = "topLeft";
 var topLeftCheck = 0;
 
@@ -27,16 +28,27 @@ var btmCenterCheck = 0;
 const bottomRight = "bottomRight"
 var btmRightCheck = 0;
 
+// Creates element for annoucing whose turn it is runs it through xMove function
+const container = document.querySelector('#turn-announcer');
+const content = document.createElement('turn');
+content.classList.add('content');
+
+
 function xMove(quadrant, xCheck) {
     if (turnNumber == 0 && xCheck == 0) {
         document.getElementById(quadrant).src="blackX.png";
         turnNumber += 1;
         xCheck += 1;
+        content.textContent = 'Player Two, Your Turn!';
+        content.setAttribute('style', 'color: black; background: red; fontSize: 40px;'); 
         return xCheck;
+
     } else if (turnNumber == 1 && xCheck == 0) {
         document.getElementById(quadrant).src="redX.png";
         turnNumber -= 1;
         xCheck += 1;
+        content.textContent = 'Player One, Your Turn!';
+        content.setAttribute('style', 'color: white; background: black; fontSize: 40px;'); 
         return xCheck;
     }
 }
@@ -67,28 +79,39 @@ function winnerCheck() {
 
 function mainFuncTopLeft() {
     topLeftCheck = xMove(topLeft, topLeftCheck);
+    container.appendChild(content);
 }
 function mainFuncTopCenter() {
     topCenterCheck = xMove(topCenter, topCenterCheck);
+    container.appendChild(content);
 }
 function mainFuncTopRight() {
     topRightCheck = xMove(topRight, topRightCheck);
+    container.appendChild(content);
 }
 function mainFuncMidLeft() {
     midLeftCheck = xMove(middleLeft, midLeftCheck);
+    container.appendChild(content);
 }
 function mainFuncMidCenter() {
     midCenterCheck = xMove(middleCenter, midCenterCheck);
+    container.appendChild(content);
 }
 function mainFuncMidRight() {
     midRightCheck = xMove(middleRight, midRightCheck);
+    container.appendChild(content);
 }
 function mainFuncBtmLeft() {
     btmLeftCheck = xMove(bottomLeft, btmLeftCheck);
+    container.appendChild(content);
 }
 function mainFuncBtmCenter() {
     btmCenterCheck = xMove(bottomCenter, btmCenterCheck);
+    container.appendChild(content);
 }
 function mainFuncBtmRight() {
     btmRightCheck = xMove(bottomRight, btmRightCheck);
+    container.appendChild(content);
 }
+
+
